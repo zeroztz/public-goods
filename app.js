3cd74692f7da205344ac2b0a11e9d62b61188fbf
Zero <zero.ztz@gmail.com>
2016-11-27 23:50:14 -0500

    Adding experiments

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.disable('etag');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('trust proxy', true);
+
+// Experiments
+app.use('/exps', require('./exps/crud'));
+
+// Participants
+app.use('/parts', require('./parts/router'));
+
+// Redirect root to /books
+app.get('/', (req, res) => {
+    res.redirect('/exps');
+});
+
+// Basic 404 handler
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use((err, req, res, next) => {
+    /* jshint unused:false */
+    console.error(err);
+    // If our routes specified a specific response, then send that. Otherwise,
+    // send a generic message so as not to leak anything.
+    res.status(500).send(err.response || 'Something broke!');
+});
+
+if (module === require.main) {
+    // Start the server
+    const server = app.listen(8080, () => {
+        const port = server.address().port;
+        console.log(`App listening on port ${port}`);
+    });
+}
+
+module.exports = app;
---
+++ exps/router.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const datastore = require('./datastore');
+
+const router = express.Router();
+
+// Automatically parse request body as form data
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Set Content-Type for all responses for these routes
+router.use((req, res, next) => {
+    res.set('Content-Type', 'text/html');
+    next();
+});
+
+/**
+ * GET /
+ *
+ * Display a page of experiments (up to ten at a time).
+ */
+router.get('/', (req, res, next) => {
+    datastore.listExpByCreationTime(10, req.query.pageToken,
+        (err, entities, cursor) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.render('exps/list.pug', {
+                exps: entities,
+                nextPageToken: cursor
+            });
+        });
+});
+
+/**
+ * GET /create
+ *
+ * Display a form for creating an experiment.
+ */
+// [START create_get]
+router.get('/create', (req, res) => {
+    res.render('exps/create.pug', {
+        credential: {}
+    });
+});
+// [END create_get]
+
+
+/**
+ * POST /create
+ *
+ * Create an experiment.
+ */
+// [START create_post]
+router.post('/create', (req, res, next) => {
+    const credential = req.body;
+
+    if (credential.passcode == "pg") {
+        // create a new experiment with current date.
+        datastore.createNewExperiment(new Date(), (err, id) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.redirect(`${req.baseUrl}/${id}`);
+        });
+    } else {
+        next(new Error(`wrong pass code${credential.passcode}`));
+    }
+});
+// [END create_post]
+
+/**
+ * GET /:exp
+ *
+ * Display an experiment.
+ */
+router.get('/:exp', (req, res, next) => {
+    datastore.readExperiment(req.params.exp, (err, entity) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('exps/view.pug', {
+            exp: entity
+        });
+    });
+});
+
+/**
+ * Errors on "/*" routes.
+ */
+router.use((err, req, res, next) => {
+    // Format error and forward to generic error handler for logging and
+    // responding to the request
+    err.response = err.message;
+    next(err);
+});
+
+module.exports = router;
