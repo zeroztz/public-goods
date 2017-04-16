e84c51d1c094e8218bb2e5b026a867f35bc3c589
Zero <zero.ztz@gmail.com>
2017-04-15 22:04:49 -0400

    add stage enum

---
+++ stage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = Object.freeze({
+    INSTRUCTION: 'instruction',
+    WAIT_FOR_COMPREHENSION: 'wait for comprehension',
+    SELECT_CONTRIBUTION: 'select contribution',
+    WAIT_FOR_SETTLEMENT: 'wait for settlement',
+    VIEW_RESULT: 'view result'
+});
