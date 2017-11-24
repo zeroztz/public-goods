ab725c6de94c25d38f1bdc7e2f5faa3974ccb1b0
Taizhi Zhang <zero.ztz@gmail.com>
2017-11-24 17:32:14 -0500

    log out inUT to test

--- storage/storage.js
+++ storage/storage.js
@@ -2,6 +2,12 @@
 
 const inUT = process.env._.endsWith("mocha")
 
+if (inUT) {
+    console.log("inUT");
+} else {
+    console.log("not inUT");
+}
+
 const {part, exp} = inUT ? require('./dummy') : require('./datastore')
 
 // [START exports]
